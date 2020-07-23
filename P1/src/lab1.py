5110cfd10239fe9260d0062e22c946d875705fef
Alicia Chew <alicia.chew@premiertaxi.com>
2020-07-23 17:12:40 +0800

    Continuation

---
+++ P1/src/lab1.py
@@ -0,0 +1,76 @@
+'''
+Created on 23 Jul 2020
+
+@author: Alicia
+'''
+
+def main():
+    '''
+    Lab 1 Question 1: Write a program that reads an input in fahrenheit and displays the temperature in centigrade.
+    '''
+#     print("This is a program to convert temperature from fahrenheit to celsius.")
+#     f = float(input("Enter a number in fahrenheit: " ))
+#     c = (5 / 9) * (f - 32)
+#     print("The temperature is" , c, "in celsius.")
+#     print() 
+    
+    '''
+    Lab 1 Question 2: Write a program that reads 3 numbers and displays the sum and average of these 3 numbers.
+    '''
+#     print("This is a program which reads 3 numbers and print the sum and average of these 3 numbers.")
+#     a = int(input("Enter first number: "))
+#     b = int(input("Enter second number: "))
+#     c = int(input("Enter third number: "))
+#     
+#     sumOfNumbers = a + b + c
+#     avgOfNumbers = sumOfNumbers / 3
+#     
+#     print("The sum of these 3 numbers is =", sumOfNumbers)
+#     print("The average of these 3 numbers is =", avgOfNumbers)
+#     print()
+    
+    '''
+    Lab 1 Question 3: Write a program that reads 3 digit integer and displays the sum and product of the digits. 
+    E.g. if the number is 123, the sum displayed is 6 and the product is also 6.
+    '''
+#     print("This is a program that takes in 3 digits.")
+#     num = int(input("Input a 3 digit integer: "))
+#     x  = num // 1000
+#     x1 = (num - x*1000) // 100
+#     x2 = (num - x*1000 - x1*100) // 10
+#     x3 = num - x*1000 - x1*100 - x2*10
+#     sumOfThreeDigits = x + x1 + x2 + x3
+#     
+#     print("The sum of digits in the number is", sumOfThreeDigits)
+#     print("The product is also", sumOfThreeDigits)
+#     print()
+    
+    '''
+    Lab 1 Question 4: Write a program that reads a positive integer representing time in seconds and converts it to hours, minutes, and seconds. 
+    E.g. if the input is 3670 seconds, output 1 hr, 1 min and 10 sec.
+    '''
+#     print("This is a program that reads a positive integer representing time in seconds and converts it to hours, minutes, and seconds.")
+#     secondsInput = int(input("Input a positive integer in seconds: "))
+#     
+#     timeInHr = secondsInput // 3600
+#     timeInMin = (secondsInput % 3600) // 60
+#     timeInSecs = (secondsInput % 3600) % 60
+# 
+#     print("The output is:", timeInHr, "hr", timeInMin, "min", timeInSecs, "secs")
+#     print()
+    
+    '''
+    Lab 1 Question 5: Write a program that reads an input representing a change which is an amount less than 1 dollar.
+    '''
+    print("This is a program that calculates the change into 50, 10, 5 and 1 cent coins.")
+    coin = int(input("Enter change that is less than 1 dollar: "))
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
